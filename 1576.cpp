db96ac7ac388737059c1b6d9491c8258f20fd82b
Swing <coco06267@163.com>
2017-09-24 14:54:05 +0800

    #1575 Tree http://hihocoder.com/problemset/problem/1575

---
+++ 1576.cpp
@@ -0,0 +1,204 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <map>
+#include <set>
+#include <bitset>
+#include <sstream>
+#include <cstdlib>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <climits>
+#include <ctype.h>
+using namespace std;
+
+#define PI acos(-1.0)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+#define eps 1e-8
+#define mod 1000000007
+
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vvi;
+
+struct Matrix {
+    vector<vector<ll>> data;
+    int r, c;
+    Matrix(int row, int col, bool identity = false) : r(row), c(col) {
+        data.assign(row, vector<ll>(col, 0));
+        if (identity) {
+            for (int i = 0; i < r; ++i) {
+                data[i][i] = 1;
+            }
+        }
+    }
+    Matrix operator * (Matrix other) {
+        int m = r, n = c, p = other.c;
+        Matrix res(m, p);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < p; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    res.data[i][j] += data[i][k] * other.data[k][j];
+                    res.data[i][j] %= mod;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+// 扩展欧几里得算法 res[1] * a + res[2] * b = res[0] = gcd(a, b)
+vector<int> extendGcd(int a, int b) {
+    if (b == 0) {
+        return {a, 1, 0};
+    } else {
+        vector<int> tmp = extendGcd(b, a % b);
+        return {tmp[0], tmp[2], tmp[1] - (a / b) * tmp[2]};
+    }
+}
+
+// 矩阵快速幂
+Matrix matrix_power(Matrix base, ll exp) {
+    int n = base.r;
+    Matrix res(n, n, true);
+    while (exp) {
+        if (exp & 1) {
+            res = res * base;
+        }
+        base = base * base;
+        exp >>= 1;
+    }
+    return res;
+}
+
+// 带模快速幂
+ll power_mod(ll base, int exp) {
+    ll res = 1;
+    while (exp) {
+        if (exp & 1) res = res * base % mod;
+        base = base * base % mod;
+        exp >>= 1;
+    }
+    return res;
+}
+
+// 快速幂
+template <class T>
+T power(T base, int exp) {
+    T res = 1;
+    while (exp) {
+        if (exp & 1) res *= base;
+        base *= base;
+        exp >>= 1;
+    }
+    return res;
+}
+
+// 求逆元
+ll inv(ll a) {
+    return power_mod(a, mod - 2);
+}
+
+#define MAXFAC 100010
+
+ll fac[MAXFAC];
+
+void initFac() {
+    fac[0] = 1;
+    for (int i = 1; i < MAXFAC; ++i) {
+        fac[i] = i * fac[i - 1] % mod;
+    }
+}
+
+ll Combine(ll a, ll b) {
+    return (fac[a] * inv(fac[b]) % mod) * inv(fac[a - b]) % mod;
+}
+
+/******************************** template ********************************/
+#define MAXN 100010
+int n;
+int self[MAXN];
+int childMin[MAXN];
+int par[MAXN];
+vector<int> children[MAXN];
+
+void build(int ind) {
+    for (int i : children[ind]) {
+        build(i);
+        childMin[ind] = min(childMin[ind], min(childMin[i], self[i]));
+    }
+}
+
+void update(int ind) {
+    if (ind == 0) return;
+    int newValue = INT_MAX;
+    for (int i : children[ind]) {
+        newValue = min(newValue, min(childMin[i], self[i]));
+    }
+    if (newValue == childMin[ind]) return;
+    if (newValue > childMin[ind]) {
+        if (self[ind] <= childMin[ind]) {
+            childMin[ind] = newValue;
+            return;
+        } else {
+            childMin[ind] = newValue;
+            update(par[ind]);
+            return;
+        }
+    } else {
+        if (self[ind] <= newValue) {
+            childMin[ind] = newValue;
+            return;
+        } else {
+            childMin[ind] = newValue;
+            update(par[ind]);
+            return;
+        }
+    }
+}
+
+int main() {
+    for (int i = 0; i < MAXN; ++i) {
+        childMin[i] = INT_MAX;
+    }
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d", &self[i]);
+    }
+    par[1] = 0;
+    for (int i = 2; i <= n; ++i) {
+        scanf("%d", &par[i]);
+        children[par[i]].pb(i);
+    }
+    build(1);
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int type;
+        scanf("%d", &type);
+        if (type == 1) {
+            int x, u;
+            scanf("%d%d", &x, &u);
+            self[x] = u;
+            update(par[x]);
+        } else {
+            int x;
+            scanf("%d", &x);
+            printf("%d\n", min(self[x], childMin[x]));
+        }
+    }
+
+    return 0;
+}
